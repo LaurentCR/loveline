307602aeb9d8120609399067e45b9a99061d73ac
Mathieu Poignet <mathieu.poignet@gmail.com>
2015-12-07 19:34:38 +0100

    converting the data into visual timeline

--- loveline.js
+++ loveline.js
@@ -8,22 +8,48 @@ function Category(label, color){
   var color = color;
 }
 
-function getLovelines(){
-  return null;
-}
 
 function createLoveline(){
-  var lovelines = getLovelines();
-  var dataTable = createDataTable(lovelines);
-  drawChart(dataTable);
+	$.getJSON("data.json", function(data){
+		var dataTable = createDataTable(data.timelines);
+  		drawChart(dataTable);
+	});
+// 	$.ajax({
+//   dataType: "json",
+//   url: "data.json",
+//   success: function(data){console.log("success")},
+//   error: function(a, b, c){console.log("failure"); console.log(a); console.log(b); console.log(c);}
+// });
+  
+  
+}
+
+function parseDate(dateString){
+	var dateTable = dateString.split('-');
+	return new Date(dateTable[0], dateTable[1], dateTable[2]);
 }
 
 function createDataTable(lovelines){
 
-  var rows = [['Category', 'Name', {role: 'style'}, 'Start', 'End']];
-  rows.push(['Foo', categories.single.label, categories.single.color, new Date(2010, 7, 1), new Date(2014, 7, 5)]);
-  rows.push(['Foo', 'Qud', '#40a67d', new Date(2014, 7, 6), new Date(2015, 7, 8)]);
-  rows.push(['Bar', 'Fiz', '#5581b4', new Date(2008, 7, 2), new Date(2014, 7, 9)]);
+	var rows = [['Category', 'Name', {role: 'style'}, 'Start', 'End']];
+
+	for(var i=0; i<lovelines.length; i++){
+		var line = lovelines[i];
+		for(var j=0; j<line.objects.length; j++){
+			var object = line.objects[j];
+			if(object.end){
+				var begin = parseDate(object.begin);
+				var end = parseDate(object.end);
+				var row = [line.name, object.type, '#40a67d', begin, end];
+				rows.push(row);
+			}
+		}
+	}
+
+  
+  // rows.push(['Foo', categories.single.label, categories.single.color, new Date(2010, 7, 1), new Date(2014, 7, 5)]);
+  // rows.push(['Foo', 'Qud', '#40a67d', new Date(2014, 7, 6), new Date(2015, 7, 8)]);
+  // rows.push(['Bar', 'Fiz', '#5581b4', new Date(2008, 7, 2), new Date(2014, 7, 9)]);
 
   return google.visualization.arrayToDataTable(rows);
 
